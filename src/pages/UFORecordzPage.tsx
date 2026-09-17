126bb62b4a62cd865c2533ba922b653d297fae1b
Fantine <fantine.mpackopriso@berkeley.edu>
2026-09-17 11:57:35 -0700

    test page UFO

--- src/pages/UFORecordzPage.tsx
+++ src/pages/UFORecordzPage.tsx
@@ -15,32 +15,10 @@ const photos = [
 
 export default function UFORecordzPage() {
   return (
-    <main className="pt-24">
-      <section className="py-24 px-6 md:px-12 lg:px-24 max-w-6xl mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <div className="md:col-span-2">
-            <img
-              src={photos[0]}
-              alt="UFO.recordz Manifestive"
-              className="w-full h-full object-cover"
-            />
-          </div>
-
-          <div className="flex flex-col gap-4">
-            <img
-              src={photos[1]}
-              alt="UFO.recordz crowd"
-              className="w-full object-cover"
-            />
-
-            <img
-              src={photos[3]}
-              alt="UFO.recordz Terminus"
-              className="w-full object-cover"
-            />
-          </div>
-        </div>
-      </section>
+    <main className="pt-32 px-12">
+      <h1 className="text-6xl">
+        UFO.RECORDZ TEST
+      </h1>
     </main>
   );
 }

2b9fa83ffaa8fe6a558a6f1f409cddaea90831e2
Fantine <fantine.mpackopriso@berkeley.edu>
2026-09-17 13:00:17 -0700

    Footer or footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import Navbar from "./components/layout/Navbar";
-import footer from "./components/layout/Footer";
+import Footer from "./components/layout/Footer";
 
 import Hero from "./sections/Hero";
 import WhoWeAre from "./sections/WhoWeAre";
@@ -42,7 +42,7 @@ export default function App() {
       {active === "events" && <EventsPage />}
       {active === "contact" && <ContactPage />}
       {active === "ufo-recordz" && <UFORecordzPage />}
-      <footer />
+      <Footer />
     </div>
   );
 }
